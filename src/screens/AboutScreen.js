0e3341bab6916f167223d21d5e9fd6e8ea2bf0d8
Phituytweepon01 <benztagum@gmail.com>
2021-02-03 10:23:04 +0700

    Deploy to Github page

---
+++ src/screens/AboutScreen.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const AboutScreen = () => {
+    return (
+        <div className="container mt-3">
+<div className="container">
+<h2 className="text-success">เกี่ยวกับ</h2>
+<p>
+เริ่มก่อตั้งเมื่อ ปี2480 ใช้ชื่อ“โรงเรียนช่างไม้” ในเนื ้อที่ของกรมการศาสนาวัดบุรินทร์(วัดร้าง) ประมาณ 6ไร่เศษ โดยมีนายจิรภัทร สถาการ เป็น ครูใหญ่ ในสมัยนั้น ปีพุทธศักราช2510
+โดยการใช้พื้นที่ของโรงเรียนการช่างสตรีซึ่งถูกยุบรวมกับ โรงเรียนช่างไม้ในเนื ้อที่15 ไร่1งาน 72 ตารางวาอันเป็นที่ ตั้งของวิทยาลัยเทคนิคในปัจจุบัน
+</p>
+</div>
+<div className="container">
+<ul type="square">
+<li>ปีพ.ศ.2480 โรงเรียนช่างไม้ตราด</li>
+<li>ปีพ.ศ.2500 โรงเรียนการช่างตราด</li>
+<li>ปีพ.ศ.2510 รวมกับ โรงเรียนการช่างสตรีใช้ชื่อโรงเรียนการช่างตราด</li>
+<li>ปีพ.ศ.2522 โรงเรียนเทคนิคตราด</li>
+<li>ปีพ.ศ.2525 ถึง ปัจจุบัน วิทยาลัยเทคนิคตราด</li>
+</ul>
+</div>
+<div className="container ml-5">
+<a href="http://www.trattc.ac.th/~nexttrat/nexttrat101.php" target="_blank"><img src="http://www.thaiwelding.com/images/stories/act_company/act2016/dec59-send-wt-trad/dec59-wt-trad-1.jpg"/></a>
+</div>
+</div>
+    )
+}
+
+export default AboutScreen
